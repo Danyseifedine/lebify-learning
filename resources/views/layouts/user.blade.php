88aa16ee436aaeb524ae85acdb0ac73da7866732
daniseifeddine <dany.a.seifeddine@gmail.com>
2024-10-25 08:09:50 +0300

    update

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -115,7 +115,7 @@
         }
     </script>
 
-    <div class="d-flex flex-column app-bg flex-root app-root" id="kt_app_root">
+    <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
             <main class="app-page flex-column flex-column-fluid" id="kt_app_page">
                 <div class="mx-5">
